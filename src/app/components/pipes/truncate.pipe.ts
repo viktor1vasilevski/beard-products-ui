e83be1e030e0e8346e4c9c71510e8d10b5c10453
vik_vas <vasilevskiviktor@yahoo.com>
2022-12-26 21:05:01 +0100

    pipe limit 140

--- src/app/components/pipes/truncate.pipe.ts
+++ src/app/components/pipes/truncate.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
 
-  transform(value: string, limit = 25, completeWords = false, ellipsis = '...')  {
+  transform(value: string, limit = 140, completeWords = false, ellipsis = '...')  {
     if (completeWords) {
       limit = value.substr(0, limit).lastIndexOf(' ');
     }
